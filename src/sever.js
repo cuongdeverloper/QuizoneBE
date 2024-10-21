f242db86a3260eb9e05cb7c10b00577e641fb653
cuongdeverloper <136320623+cuongdeverloper@users.noreply.github.com>
2024-10-21 23:42:55 +0700

    jade

--- src/sever.js
+++ src/sever.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const configViewEngine = require('./config/ViewEngine.js');
 const cors = require('cors');
-const path = require('path');
-
 require('dotenv').config();
 const cookieParser = require('cookie-parser');
 const session = require('express-session');
